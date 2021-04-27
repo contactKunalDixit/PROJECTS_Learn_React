601fa5d8b7a415629cf0aeb8b034690e9921d389
Kunal Dixit <contactkunaldixit@gmail.com>
2021-04-28 03:02:58 +0800

    first commit

---
+++ 1_Getting_Started/Notes.js
@@ -0,0 +1,30 @@
+// todo:    Module 1:  Welcome to the course
+
+/*
+
+!   Chapter 1 to 10 - Summary ***************
+
+?   Q: What is React.js ?
+
+*   React is a Javascript library for building User Interfaces
+React is a popular JavaScript client's side library for building reusable, component driven user Interfaces for Web pages or applications.
+React combines HTML with JavaScript functionality into its own markup language called as 'JSX'
+React also makes it easy to manage the flow of data throughout the application.
+React is an open source library created and maintained by facebook. It's a great tool to render the User interface (UI) of modern web applications.
+
+Generally, Mobile apps and Desktop apps (say Netflix) feels a lot different from web pages or web apps in terms of how 'reactive' and 'responsive' they feel. Things happen instantly, you don't wait for new pages to ;load or actions to start.
+
+Tradionally, in web apps or web pages, you click a link and wait for a new page to load. You click a  button and wait for some action to complete. That's because on the backend, what the click triggers, is a request from user's end (browser in this case) and sends it to the server. The server in return, and as a response sends the new HTML file with new data.
+
+This entire long 'request respond' cycle can be broken and can be curtailed with JavaScript as it facilitates DOM manipulation. That means, we can have the new data show up at user's end through DOM manipulation without fetching or requesting a new data from the server. We dont need to request a new HTML page (and wait for the response) as the 'code' written by dev in JavaScript file can manipulate and show the updated and intended data.   
+
+
+Netflix app is a good example of a react application. It runs very smoothly and gives an absolute feel of being a Mob application even being opened on web browser. It gives higher and better user interaction feel and never lags when moving from one page or section to another. 
+That is so because, in case of a normal website, the user types a URl in the browser and the requests is sent to the server which in return sends an appropriate response with the relevant HTML, CSS and javaScript files, which the browser then renders on the screen for the user.
+Javascript to a certain extent can let the developer have control through prograaming for DOM manipulation in the JS file itself.
+
+?   Q:  Why use React instead of just "JavaScript" ?
+
+
+
+*/
