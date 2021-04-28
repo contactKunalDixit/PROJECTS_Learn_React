35fa3aff1a6dd38203407752131567f455fb7dea
Kunal Dixit <contactkunaldixit@gmail.com>
2021-04-29 03:05:31 +0800

    Updated

--- 1_Getting_Started/Notes.js
+++ 1_Getting_Started/Notes.js
@@ -20,7 +20,7 @@ React is an open source library created and maintained by facebook. It's a great
 
 Generally, Mobile apps and Desktop apps (say Netflix) feels a lot different from web pages or web apps in terms of how 'reactive' and 'responsive' they feel. Things happen instantly, you don't wait for new pages to ;load or actions to start.
 
-Tradionally, in web apps or web pages, you click a link and wait for a new page to load. You click a  button and wait for some action to complete. That's because on the backend, what the click triggers, is a request from user's end (browser in this case) and sends it to the server. The server in return, and as a response sends the new HTML file with new data.
+Tradionally, in web apps or web pages, you click a link and wait for a new page to load. You click a  button and wait for some action tChapter 1 to 10 - Summary ***************o complete. That's because on the backend, what the click triggers, is a request from user's end (browser in this case) and sends it to the server. The server in return, and as a response sends the new HTML file with new data.
 
 This entire long 'request respond' cycle can be broken and can be curtailed with JavaScript as it facilitates DOM manipulation. That means, we can have the new data show up at user's end through DOM manipulation without fetching or requesting a new data from the server. We dont need to request a new HTML page (and wait for the response) as the 'code' written by dev in JavaScript file can manipulate and show the updated and intended data.   
 
@@ -63,4 +63,3 @@ Vue.js: Vue.js stands somewhere in between Angular and React or one may say that
 
 */
 
-let name = kunal
---
+++ 2_JavaScript_Refresher/Notes.js
@@ -0,0 +1,11 @@
+// todo:    Module 2:  JavaScript Refresher
+
+/*
+? What should you know first?
+
+? Data Types, variables, functions, loops etc
+? Promises & Asynchronous programming
+? Array methods like forEach() & map()
+? Fetch API & making HTTP requests
+
+!   Chapter 11 to 23 - Summary ***************
