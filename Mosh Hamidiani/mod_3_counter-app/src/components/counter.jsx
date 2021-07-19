f0c6154449175e5b2a7966d3d1ac8de5d3aa7805
Kunal Dixit <contactkunaldixit@gmail.com>
2021-07-20 04:50:35 +0800

    Updated

--- Mosh Hamidiani/mod_3_counter-app/src/components/counter.jsx
+++ Mosh Hamidiani/mod_3_counter-app/src/components/counter.jsx
@@ -7,38 +7,37 @@ class Counter extends Component {
 	// 	this.handleIncrement = this.handleIncrement.bind(this); //or else, in order to avoid binding of each event handler like this, USE ARROW function
 	// }
 
-	state = {
-		value: this.props.value,
-	};
-
 	formatCount() {
-		const { value } = this.state;
+		const { value } = this.props.countersItem;
 		return value === 0 ? 'Zero' : value;
 	}
 	getBadgeClasses() {
 		let classes = 'badge m-2 badge-';
-		classes += this.state.value === 0 ? 'warning' : 'primary';
+		classes += this.props.countersItem.value === 0 ? 'warning' : 'primary';
 		return classes;
 	}
 
-	handleIncrement = (placeHolder) => {
-		this.setState({ value: this.state.value + 1 });
-	};
-
 	render() {
-		console.log('props', this.props);
 		return (
 			<div>
-				<h4>Counter# {this.props.id}</h4>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button
-					// onClick={this.handleIncrement}
+					// onClick={this.handleIncrement}  Instead of this, Inm order to pass an arguement, Do below:
 					onClick={() => {
-						this.handleIncrement();
+						this.props.onIncrement(this.props.countersItem);
 					}}
 					className='btn btn-secondary btn-sm'
 				>
 					Increment
+				</button>
+				<button
+					onClick={() => {
+						console.log('this file is');
+						return this.props.onDelete(this.props.countersItem.id);
+					}}
+					className='btn btn-danger btn-sm m-2'
+				>
+					Delete
 				</button>
 			</div>
 		);
--- Mosh Hamidiani/mod_3_counter-app/src/components/counters.jsx
+++ Mosh Hamidiani/mod_3_counter-app/src/components/counters.jsx
@@ -11,15 +11,61 @@ class Counters extends Component {
 			{ id: 5, value: 0 },
 		],
 	};
+
+	handleIncrement = (countersItem) => {
+		console.log(countersItem);
+		const newCounters = [...this.state.counters];
+		//spreading: cloning the array using spread operators, because we cant make changes to the original state directly..
+
+		const index = newCounters.indexOf(countersItem);
+		// finding index value of the specific object from newCounters array;
+
+		newCounters[index] = { ...countersItem };
+		// destructuring : the specific object recieved through uplifting ..Establishing the specific uplifted object's clone that gets to reside at that specific index position
+
+		newCounters[index].value++;
+		// incrementing the value property of the object (by 1) positioned at that index value
+
+		this.setState({ counters: newCounters });
+		// Updating the state
+	};
+
+	handleDelete = (counterId) => {
+		console.log('Event handler called', counterId);
+
+		const newCounters = this.state.counters.filter((itemObj) => {
+			return itemObj.id !== counterId;
+		});
+
+		this.setState({ counters: newCounters });
+	};
+
+	handleReset = () => {
+		const resettedCounters = this.state.counters.map((i) => {
+			i.value = 0;
+			return i;
+		});
+		this.setState({ counters: resettedCounters });
+	};
+
 	render() {
+		if (this.state.counters.length === 0) {
+			return <h4>There is noting to show in the list..Pls refresh</h4>;
+		}
 		return (
 			<div>
-				{this.state.counters.map((counterItem) => (
+				<button
+					onClick={this.handleReset}
+					className='btn btn-sm btn-primary m-2'
+				>
+					Reset
+				</button>
+				{this.state.counters.map((countersItem) => (
 					<Counter
-						key={counterItem.id}
-						value={counterItem.value}
-						selected={true}
-						id={counterItem.id}
+						key={countersItem.id}
+						onDelete={this.handleDelete}
+						onIncrement={this.handleIncrement}
+						countersItem={countersItem}
 					></Counter>
 				))}
 			</div>
