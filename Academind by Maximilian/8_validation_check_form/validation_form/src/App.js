09e9fe012ba62c6f095192a462147c7723f216f5
Kunal Dixit <contactkunaldixit@gmail.com>
2021-08-11 00:44:44 +0800

    Updated - UsersList + ErrorModal

--- Academind by Maximilian/8_validation_check_form/validation_form/src/App.js
+++ Academind by Maximilian/8_validation_check_form/validation_form/src/App.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import './App.css';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+	const [usersList, setUsersList] = useState([]);
+
+	const addUsersHandler = (uName, uAge) => {
+		setUsersList((prevUsersList) => {
+			return [...prevUsersList, { name: uName, age: uAge }];
+		});
+	};
 	return (
 		<div>
-			<AddUser />
+			<AddUser onAddUser={addUsersHandler} />
+			<UsersList users={usersList} />
 		</div>
 	);
 }
--- Academind by Maximilian/8_validation_check_form/validation_form/src/components/Users/AddUser.jsx
+++ Academind by Maximilian/8_validation_check_form/validation_form/src/components/Users/AddUser.jsx
@@ -1,20 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import style from './AddUser.module.css';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+
 const AddUser = (props) => {
 	const [enteredUserName, setEnteredUserName] = useState('');
 	const [enteredAge, setEnteredAge] = useState('');
-
+	const [error, setError] = useState(); // initially, the 'error' state is left 'null'
 	const AddUserHandler = (e) => {
 		e.preventDefault();
+
+		if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+			setError({
+				title: 'Invalid Input',
+				message:
+					'Please use alphabets in userName and number greater than 1 for your Age',
+			});
+			return;
+		}
+		if (+enteredAge < 1) {
+			// converting the userinput which'll always be in string to a number by prefixing '+' operator
+			setError({
+				title: 'Invalid Input',
+				message:
+					'Please use alphabets in userName and number greater than 1 for your Age',
+			});
+			return;
+		}
 		console.log(enteredUserName, enteredAge);
+
+		props.onAddUser(enteredUserName, enteredAge);
+		setEnteredUserName('');
+		setEnteredAge('');
 	};
 
-	useEffect(() => {
-		setEnteredAge('');
-		setEnteredUserName('');
-	}, []);
 	const userNameChangeHandler = (e) => {
 		setEnteredUserName(e.target.value);
 	};
@@ -23,30 +43,43 @@ const AddUser = (props) => {
 		setEnteredAge(e.target.value);
 	};
 
+	const errorHandler = () => {
+		setError(null);
+	};
 	return (
-		<Card className={style.input}>
-			<form
-				onSubmit={(e) => {
-					AddUserHandler(e);
-				}}
-			>
-				<label htmlFor='username'>UserName: </label>
-				<input
-					type='text'
-					id='username'
-					onChange={userNameChangeHandler}
-					value={enteredUserName}
-				></input>
-				<label htmlFor='age'>Age(Years):</label>
-				<input
-					type='text'
-					id='age'
-					onChange={userAgeChangeHandler}
-					value={enteredAge}
-				></input>
-				<Button type='submit'>Add User</Button>
-			</form>
-		</Card>
+		<>
+			{/* If 'error' is true, only then the ErrorModal would be rendered, Else Not */}
+			{error && (
+				<ErrorModal
+					title={error.title}
+					message={error.message}
+					onConfirm={errorHandler}
+				></ErrorModal>
+			)}
+			<Card className={style.input}>
+				<form
+					onSubmit={(e) => {
+						AddUserHandler(e);
+					}}
+				>
+					<label htmlFor='username'>UserName: </label>
+					<input
+						type='text'
+						id='username'
+						onChange={userNameChangeHandler}
+						value={enteredUserName}
+					></input>
+					<label htmlFor='age'>Age(Years):</label>
+					<input
+						type='text'
+						id='age'
+						onChange={userAgeChangeHandler}
+						value={enteredAge}
+					></input>
+					<Button type='submit'>Add User</Button>
+				</form>
+			</Card>
+		</>
 	);
 };
 
