c2cd3cc0343b174520de3de6d9fd9f29ee441571
Kunal Dixit <contactkunaldixit@gmail.com>
2021-08-22 00:51:37 +0800

    Update = widgetLg+ Status buttons Auto Color Logic

--- Portfolio_FireItems/React_Admin_Dashboard/react_admin-dashboard/src/components/widgetLarge/WidgetLg.jsx
+++ Portfolio_FireItems/React_Admin_Dashboard/react_admin-dashboard/src/components/widgetLarge/WidgetLg.jsx
@@ -2,7 +2,99 @@ import React from 'react';
 import styles from './widgetLg.module.css';
 
 const WidgetLg = () => {
-	return <div className={styles.widgetLg}>Large</div>;
+	const ButtonLG = ({ type }) => {
+		return (
+			<button className={`${styles.widgetLgButton} ${type}`}>{type}</button>
+		);
+	};
+	return (
+		<div className={styles.widgetLg}>
+			<span className={styles.widgetLgTitle}>Latest Transactions</span>
+			<table classNAme={styles.widgetLgTable}>
+				<tr className={styles.widgetLgTr}>
+					<th className={styles.widgetLgTh}>Customer</th>
+					<th className={styles.widgetLgTh}>Date</th>
+					<th className={styles.widgetLgTh}>Amount</th>
+					<th className={styles.widgetLgTh}>Status</th>
+				</tr>
+				<tr className={styles.widgetLgTr}>
+					<td className={styles.widgetLgUser}>
+						<img
+							src='https://images.pexels.com/photos/5704720/pexels-photo-5704720.jpeg?cs=srgb&dl=pexels-anastasia-shuraeva-5704720.jpg&fm=jpg'
+							alt=''
+							className={styles.widgetLgImg}
+						/>
+						<span classname={styles.widgetLgName}>Susan Carol</span>
+					</td>
+					<td className={styles.widgetLgDate}>2 Jun 2021</td>
+					<td className={styles.widgetLgAmount}>$122.00</td>
+					<td className={styles.widgetLgStatus}>
+						<ButtonLG type={'Approved'} />
+					</td>
+				</tr>
+				<tr className={styles.widgetLgTr}>
+					<td className={styles.widgetLgUser}>
+						<img
+							src='https://images.pexels.com/photos/5704720/pexels-photo-5704720.jpeg?cs=srgb&dl=pexels-anastasia-shuraeva-5704720.jpg&fm=jpg'
+							alt=''
+							className={styles.widgetLgImg}
+						/>
+						<span classname={styles.widgetLgName}>Susan Carol</span>
+					</td>
+					<td className={styles.widgetLgDate}>2 Jun 2021</td>
+					<td className={styles.widgetLgAmount}>$122.00</td>
+					<td className={styles.widgetLgStatus}>
+						<ButtonLG type={'Declined'} />
+					</td>
+				</tr>
+				<tr className={styles.widgetLgTr}>
+					<td className={styles.widgetLgUser}>
+						<img
+							src='https://images.pexels.com/photos/5704720/pexels-photo-5704720.jpeg?cs=srgb&dl=pexels-anastasia-shuraeva-5704720.jpg&fm=jpg'
+							alt=''
+							className={styles.widgetLgImg}
+						/>
+						<span classname={styles.widgetLgName}>Susan Carol</span>
+					</td>
+					<td className={styles.widgetLgDate}>2 Jun 2021</td>
+					<td className={styles.widgetLgAmount}>$122.00</td>
+					<td className={styles.widgetLgStatus}>
+						<ButtonLG type={'Pending'} />
+					</td>
+				</tr>
+				<tr className={styles.widgetLgTr}>
+					<td className={styles.widgetLgUser}>
+						<img
+							src='https://images.pexels.com/photos/5704720/pexels-photo-5704720.jpeg?cs=srgb&dl=pexels-anastasia-shuraeva-5704720.jpg&fm=jpg'
+							alt=''
+							className={styles.widgetLgImg}
+						/>
+						<span classname={styles.widgetLgName}>Susan Carol</span>
+					</td>
+					<td className={styles.widgetLgDate}>2 Jun 2021</td>
+					<td className={styles.widgetLgAmount}>$122.00</td>
+					<td className={styles.widgetLgStatus}>
+						<ButtonLG type={'Approved'} />
+					</td>
+				</tr>
+				<tr className={styles.widgetLgTr}>
+					<td className={styles.widgetLgUser}>
+						<img
+							src='https://images.pexels.com/photos/5704720/pexels-photo-5704720.jpeg?cs=srgb&dl=pexels-anastasia-shuraeva-5704720.jpg&fm=jpg'
+							alt=''
+							className={styles.widgetLgImg}
+						/>
+						<span classname={styles.widgetLgName}>Susan Carol</span>
+					</td>
+					<td className={styles.widgetLgDate}>2 Jun 2021</td>
+					<td className={styles.widgetLgAmount}>$122.00</td>
+					<td className={styles.widgetLgStatus}>
+						<ButtonLG type={'Approved'} />
+					</td>
+				</tr>
+			</table>
+		</div>
+	);
 };
 
 export default WidgetLg;
--- Portfolio_FireItems/React_Admin_Dashboard/react_admin-dashboard/src/components/widgetSmall/WidgetSm.jsx
+++ Portfolio_FireItems/React_Admin_Dashboard/react_admin-dashboard/src/components/widgetSmall/WidgetSm.jsx
@@ -17,8 +17,54 @@ const WidgetSm = () => {
 						<span className={styles.widgetSmUserName}>Anna Shultz</span>
 						<span className={styles.widgetSmUserTitle}>Software Developer</span>
 					</div>
-					<button>
-						<Visibility />
+					<button className={styles.widgetSmButton}>
+						<Visibility fontSize='small' style={{ marginRight: '5px' }} />
+						Display
+					</button>
+				</li>
+				<li className={styles.widgetSmListItem}>
+					<img
+						src='https://images.pexels.com/photos/8417206/pexels-photo-8417206.jpeg?cs=srgb&dl=pexels-shvets-production-8417206.jpg&fm=jpg'
+						alt=''
+						className={styles.widgetSmImg}
+					/>
+					<div className={styles.widgetSmUser}>
+						<span className={styles.widgetSmUserName}>Anna Shultz</span>
+						<span className={styles.widgetSmUserTitle}>Software Developer</span>
+					</div>
+					<button className={styles.widgetSmButton}>
+						<Visibility fontSize='small' style={{ marginRight: '5px' }} />
+						Display
+					</button>
+				</li>
+				<li className={styles.widgetSmListItem}>
+					<img
+						src='https://images.pexels.com/photos/8417206/pexels-photo-8417206.jpeg?cs=srgb&dl=pexels-shvets-production-8417206.jpg&fm=jpg'
+						alt=''
+						className={styles.widgetSmImg}
+					/>
+					<div className={styles.widgetSmUser}>
+						<span className={styles.widgetSmUserName}>Anna Shultz</span>
+						<span className={styles.widgetSmUserTitle}>Software Developer</span>
+					</div>
+					<button className={styles.widgetSmButton}>
+						<Visibility fontSize='small' style={{ marginRight: '5px' }} />
+						Display
+					</button>
+				</li>
+				<li className={styles.widgetSmListItem}>
+					<img
+						src='https://images.pexels.com/photos/8417206/pexels-photo-8417206.jpeg?cs=srgb&dl=pexels-shvets-production-8417206.jpg&fm=jpg'
+						alt=''
+						className={styles.widgetSmImg}
+					/>
+					<div className={styles.widgetSmUser}>
+						<span className={styles.widgetSmUserName}>Anna Shultz</span>
+						<span className={styles.widgetSmUserTitle}>Software Developer</span>
+					</div>
+					<button className={styles.widgetSmButton}>
+						<Visibility fontSize='small' style={{ marginRight: '5px' }} />
+						Display
 					</button>
 				</li>
 			</ul>
