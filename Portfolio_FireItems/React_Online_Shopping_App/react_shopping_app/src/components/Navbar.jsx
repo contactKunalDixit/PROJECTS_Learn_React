d67fba56f18d9d4ab60141f15a4c8ab69d6cdc0c
Kunal Dixit <contactkunaldixit@gmail.com>
2021-09-11 04:48:02 +0800

    Updated Navbar+Announcement+Slider - workInProg

--- Portfolio_FireItems/React_Online_Shopping_App/react_shopping_app/src/components/Navbar.jsx
+++ Portfolio_FireItems/React_Online_Shopping_App/react_shopping_app/src/components/Navbar.jsx
@@ -1,7 +1,113 @@
 import React from 'react';
+import styled from 'styled-components';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import LovelyBees from '../Img/lovely-bees.png';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+
+const Container = styled.div`
+	height: 60px;
+`;
+
+const Wrapper = styled.div`
+	padding: 10px 20px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Left = styled.div`
+	flex: 1;
+	display: flex;
+	align-items: center;
+`;
+
+const Language = styled.span`
+	font-size: 14px;
+	cursor: pointer;
+`;
+const SearchContainer = styled.div`
+	border: 0.5px solid gray;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-left: 25px;
+	padding: 5px;
+	border-radius: 10px;
+`;
+
+const Input = styled.input`
+	border: none;
+`;
+
+const Center = styled.div`
+	flex: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Logo = styled.h1`
+	font-weight: bold;
+`;
+
+const LogoIcon = styled.img`
+	width: 60px;
+	height: 60px;
+	position: relative;
+	bottom: 10px;
+`;
+
+const Right = styled.div`
+	flex: 1;
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+`;
+
+const MenuItem = styled.div`
+	font-size: 14px;
+	cursor: pointer;
+	margin: 0px 25px;
+`;
 
 const Navbar = () => {
-	return <div>navbar</div>;
+	return (
+		<>
+			<Container>
+				<Wrapper>
+					<Left>
+						<Language>EN</Language>
+						<SearchContainer>
+							<Input />
+							<SearchOutlinedIcon style={{ fontSize: '16px', color: 'grey' }} />
+						</SearchContainer>
+					</Left>
+					<Center>
+						<Logo>BuyBee</Logo>
+
+						<LogoIcon src={LovelyBees}></LogoIcon>
+					</Center>
+					<Right>
+						<MenuItem>Register</MenuItem>
+						<MenuItem>Sign In</MenuItem>
+						<MenuItem>
+							<Badge badgeContent={4} color='primary'>
+								<ShoppingCartOutlinedIcon />
+							</Badge>
+						</MenuItem>
+					</Right>
+				</Wrapper>
+			</Container>
+		</>
+	);
 };
+
+/* create a Credit footer on the last page :
+ PLEASE give credit to author for using the 'Bee' icon:
+
+ <a href='https://lovepik.com/images/png-animals.html'>
+Animals Png vectors by Lovepik.com
+</a> */
 
 export default Navbar;
---
+++ Portfolio_FireItems/React_Online_Shopping_App/react_shopping_app/src/components/Slider.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import styled from 'styled-components';
+import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined';
+import ArrowForwardIosOutlinedIcon from '@material-ui/icons/ArrowForwardIosOutlined';
+
+const Container = styled.div`
+	width: 100%;
+	/* height: calc(100vh - 90px); */
+	height: 100vh;
+	display: flex;
+
+	position: relative;
+	/* overflow: hidden; */
+`;
+
+const Arrow = styled.div`
+	width: 50px;
+	height: 50px;
+	background-color: #fcf4f4;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 50%;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	margin: auto; /*For centering the arrows vertically: top, bottom:0 and margin:Auto*/
+	left: ${(props) => props.direction === 'Left' && '10px'};
+	right: ${(props) => props.direction === 'Right' && '10px'};
+	cursor: pointer;
+	opacity: 0.5;
+	z-index: 100;
+`;
+
+const Wrapper = styled.div`
+	height: 100%;
+	display: flex;
+`;
+
+const SlideContainer = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	background-color: #${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+	flex: 1;
+	height: 100%;
+`;
+
+const Image = styled.img`
+	height: 80%;
+`;
+
+const InfoContainer = styled.div`
+	flex: 1;
+	padding: 50px;
+`;
+
+const Title = styled.h1`
+	font-size: 70px;
+`;
+const Desc = styled.p`
+	margin: 50px 0px;
+	font-size: 20px;
+	font-weight: 500;
+	letter-spacing: 2px;
+`;
+const Button = styled.button`
+	padding: 10px;
+	font-size: 20px;
+	background-color: transparent;
+	letter-spacing: 1px;
+	cursor: pointer;
+`;
+
+const Slider = () => {
+	return (
+		<Container>
+			<Arrow direction='Left'>
+				<ArrowBackIosOutlinedIcon style={{ color: 'black' }} />
+			</Arrow>
+			<Wrapper>
+				<SlideContainer bg='f5fafd'>
+					<ImgContainer>
+						<Image src='https://i.ibb.co/XsdmR2c/1.png' />
+					</ImgContainer>
+					<InfoContainer>
+						<Title>Summer Sale</Title>
+						<Desc>
+							DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF ON NEW ARRIVALS
+						</Desc>
+						<Button>SHOP NOW</Button>
+					</InfoContainer>
+				</SlideContainer>
+				<SlideContainer bg='fcf1ed'>
+					<ImgContainer>
+						<Image src='https://i.ibb.co/XsdmR2c/1.png' />
+					</ImgContainer>
+					<InfoContainer>
+						<Title>Winter Sale</Title>
+						<Desc>
+							DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF ON NEW ARRIVALS
+						</Desc>
+						<Button>SHOP NOW</Button>
+					</InfoContainer>
+				</SlideContainer>
+				<SlideContainer bg='fbf0f4'>
+					<ImgContainer>
+						<Image src='https://i.ibb.co/XsdmR2c/1.png' />
+					</ImgContainer>
+					<InfoContainer>
+						<Title>Popular Sale</Title>
+						<Desc>
+							DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF ON NEW ARRIVALS
+						</Desc>
+						<Button>SHOP NOW</Button>
+					</InfoContainer>
+				</SlideContainer>
+			</Wrapper>
+			<Arrow direction='Right'>
+				<ArrowForwardIosOutlinedIcon style={{ color: 'black' }} />
+			</Arrow>
+		</Container>
+	);
+};
+
+export default Slider;
