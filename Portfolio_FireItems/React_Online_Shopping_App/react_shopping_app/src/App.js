22c6a116e819534353f8e5b07ee66aac67d4a15b
Kunal Dixit <contactkunaldixit@gmail.com>
2021-09-13 04:56:14 +0800

    Updated

--- Portfolio_FireItems/React_Online_Shopping_App/react_shopping_app/src/App.js
+++ Portfolio_FireItems/React_Online_Shopping_App/react_shopping_app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 function App() {
 	return (
 		<div>
-			<Home></Home>
+			{/* <Home /> */}
+			<ProductList />
 		</div>
 	);
 }
