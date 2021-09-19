0276b732ab7ef2477d3cf3694ec1582921568971
Kunal Dixit <contactkunaldixit@gmail.com>
2021-09-19 15:12:03 +0800

    Structure ready - parallex to be added

--- Portfolio_FireItems/Rellax_React_Landing_Page/rellax_react_page/src/components/Footer.jsx
+++ Portfolio_FireItems/Rellax_React_Landing_Page/rellax_react_page/src/components/Footer.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	padding: 10px 50px;
+	margin-top: 1px;
+	background-color: #2e2356;
+`;
+
+const ItemContainer = styled.ul`
+	list-style: none;
+	padding: 0;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Item = styled.li`
+	color: white;
+	font-size: 12px;
+`;
 
 const Footer = () => {
-	return <div></div>;
+	return (
+		<Wrapper>
+			<ItemContainer>
+				<Item>FAQ</Item>
+				<Item>Terms of Use</Item>
+				<Item>Primary Notice</Item>
+				<Item>Contact Us</Item>
+				<Item>About Us</Item>
+			</ItemContainer>
+		</Wrapper>
+	);
 };
 
 export default Footer;
--- Portfolio_FireItems/Rellax_React_Landing_Page/rellax_react_page/src/pages/Home.jsx
+++ Portfolio_FireItems/Rellax_React_Landing_Page/rellax_react_page/src/pages/Home.jsx
@@ -1,40 +1,166 @@
 import React from 'react';
 import styled from 'styled-components';
 import Footer from '../components/Footer';
+import Play from '../Images/play-button.png';
+
+// import VideoCameraFrontIcon from '@material-ui/icons/VideoCameraFrontOutlined';
+// import GroupsOutlinedIcon from '@material-ui/icons/GroupsOutlinedIcon';
+// import ShareOutlinedIcon from '@material-ui/icons/ShareOutlinedIcon';
+
+// import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Container = styled.div`
-	background-color: red;
-	width: 100vw;
+	width: 100%;
+	background-color: #2e2356;
 `;
 const Top = styled.div`
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-image: url('https://i.ibb.co/PhVR2Vh/bg1.png');
+	background-size: cover;
+`;
+
+const TopLeft = styled.div`
+	flex: 1.5;
+`;
+
+const TopRight = styled.div`
+	flex: 2;
+`;
+const Title = styled.h1`
+	color: #${(props) => (props.color === 'white' ? `ffffff` : `f5a676`)};
+	font-size: ${(props) => props.color === 'white' && `${props.size}`};
+	font-weight: 400;
+	/* letter-spacing: 1px; */
+	margin: ${(props) => (props.color !== 'white' ? `10px` : `30px`)} 0px;
+	opacity: 100%;
+`;
+const ButtonTop = styled.button`
+	width: 20%;
+	padding: 10px;
+	font-size: 14px;
+	border-radius: 5px;
+
+	margin-top: 50px;
+	&&:hover {
+		background-color: #f3aeae;
+		transform: scale(0.95);
+		transition: all 0.8s ease;
+	}
+`;
+
+const Middle = styled.div`
+	height: 80vh;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 `;
-const LeftSec = styled.div`
+const MiddleLeft = styled.div`
 	flex: 1;
-	background-color: red;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
-const RightSec = styled.div`
-	flex: 2;
-	background-color: #4118d4;
+
+const Image = styled.img`
+	width: 50%;
+	object-fit: cover;
 `;
-const Middle = styled.div``;
-const Bottom = styled.div``;
+const MiddleRight = styled.div`
+	flex: 1;
+	height: 80%;
+	display: flex;
+	justify-content: space-evenly;
+	flex-direction: column;
+	align-items: center;
+`;
+const SubSection = styled.div`
+	opacity: 80%;
+	border-radius: 5px;
+	border: 1px solid #f3aeae;
+	padding: 0 10px;
+	margin: 20px 20px;
+`;
+const Desc = styled.p`
+	color: white;
+`;
+
+const Bottom = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 60vh;
+`;
 
 const Home = () => {
 	return (
 		<>
 			<Container>
 				<Top>
-					<LeftSec>Heeeee</LeftSec>
-					<RightSec>Haaaa</RightSec>
+					<TopLeft></TopLeft>
+					<TopRight>
+						<Title color='white' size='60px'>
+							Community Based Learning
+						</Title>
+						<ButtonTop>Learn More</ButtonTop>
+					</TopRight>
 				</Top>
 				<Middle>
-					<LeftSec>Hello</LeftSec>
-					<RightSec>World</RightSec>
+					<MiddleLeft>
+						<Image src={Play}></Image>
+					</MiddleLeft>
+					<MiddleRight>
+						<SubSection>
+							<Title margin='10px'>Stream Everything</Title>
+							<Desc>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Corporis dolor neque distinctio suscipit ducimus, perferend
+								margin-bottom: 35px;is incidunt odit maiores soluta? Dolorem ea
+								iure reiciendis illum voluptas!
+							</Desc>
+						</SubSection>
+						<SubSection>
+							<Title margin='10px'>Short is the New Long</Title>
+							<Desc>
+								Lorem ipsum dolor sit amet consectetur margin: '20px
+								20px'adipisicing elit. Corporis dolor neque distinctio suscipit
+								ducimus, perferendis incidunt odit maiores soluta? Dolorem ea
+								iure reiciendis illum voluptas!
+							</Desc>
+						</SubSection>
+					</MiddleRight>
 				</Middle>
-				<Bottom></Bottom>
+				<Bottom>
+					<SubSection>
+						{/* <VideoCameraFrontIcon /> */}
+						<Title>Watch</Title>
+						<Desc>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+							ipsum dolor sit amet consectetur adipisicing elit. Velit,
+							consequuntur.
+						</Desc>
+					</SubSection>
+					<SubSection>
+						{/* <GroupsOutlinedIcon /> */}
+						<Title>Learn</Title>{' '}
+						<Desc>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+							ipsum dolor sit amet consectetur adipisicing elit. Velit,
+							consequuntur.
+						</Desc>
+					</SubSection>
+					<SubSection>
+						{/* <ShareOutlinedIcon /> */}
+						<Title>Share</Title>{' '}
+						<Desc>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+							ipsum dolor sit amet consectetur adipisicing elit. Velit,
+							consequuntur.
+						</Desc>
+					</SubSection>
+				</Bottom>
 			</Container>
 			<Footer></Footer>
 		</>
