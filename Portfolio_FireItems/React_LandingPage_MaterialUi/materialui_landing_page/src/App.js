e42057748dcfa62a2d562b548d05f76d29c4e8da
Kunal Dixit <contactkunaldixit@gmail.com>
2021-09-22 15:49:34 +0800

    Updated initial file structure + dependencies

---
+++ Portfolio_FireItems/React_LandingPage_MaterialUi/materialui_landing_page/src/App.js
@@ -0,0 +1,12 @@
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+
+function App() {
+	return (
+		<>
+			<AccessAlarmIcon />
+			<div>Hello World</div>
+		</>
+	);
+}
+
+export default App;
