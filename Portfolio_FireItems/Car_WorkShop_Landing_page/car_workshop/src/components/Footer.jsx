ac7f06c24242305931b911c16bf986a7fa04fedc
Kunal Dixit <contactkunaldixit@gmail.com>
2021-11-07 13:59:56 +0800

    Updated UI

--- Portfolio_FireItems/Car_WorkShop_Landing_page/car_workshop/src/components/Footer.jsx
+++ Portfolio_FireItems/Car_WorkShop_Landing_page/car_workshop/src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './footer.module.css';
 
 const Footer = () => {
-	return <div className={styles.main}></div>;
+	return (
+		<div className={styles.footer}>
+			<div className={styles.left}>&copy; KunalD.</div>
+			<div className={styles.right}>Ctc: +65 90301206</div>
+		</div>
+	);
 };
 
 export default Footer;
--- Portfolio_FireItems/Car_WorkShop_Landing_page/car_workshop/src/components/Main.jsx
+++ Portfolio_FireItems/Car_WorkShop_Landing_page/car_workshop/src/components/Main.jsx
@@ -2,20 +2,7 @@ import React from 'react';
 import styles from './main.module.css';
 
 const Main = () => {
-	return (
-		<div
-			className={styles.main}
-			style={{
-				backgroundImage:
-					'url(' +
-					'https://raw.githubusercontent.com/contactKunalDixit/Proj8_PU_Automotive_car_Design/main/guille-sanchez-9nxrdT2SMCg-unsplash%20(1).jpg' +
-					')',
-				backgroundPosition: 'center',
-				backgroundSize: 'cover',
-				backgroundRepeat: 'no-repeat',
-			}}
-		></div>
-	);
+	return <div className={styles.main}></div>;
 };
 
 export default Main;
