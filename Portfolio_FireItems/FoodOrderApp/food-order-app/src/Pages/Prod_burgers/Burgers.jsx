a6fc2ce4b32fdf5f0ba3175c21b5d0c6f086f40a
Kunal Dixit <contactkunaldixit@gmail.com>
2021-11-18 00:21:22 +0800

    Updated ProdBurger page

--- Portfolio_FireItems/FoodOrderApp/food-order-app/src/Pages/Prod_burgers/Burgers.jsx
+++ Portfolio_FireItems/FoodOrderApp/food-order-app/src/Pages/Prod_burgers/Burgers.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './burgers.module.css';
-
+import { productBurger } from '../../data';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { GrSubtractCircle } from 'react-icons/gr';
 const Burgers = () => {
 	return (
 		<div className={styles.Burgers}>
@@ -12,41 +14,50 @@ const Burgers = () => {
 
 				<div className={styles.bottom}>
 					<div className={styles.scrolling_wrapper_flexbox}>
-						<div className={styles.card}>
-							<h2>Card</h2>
-							<div className={styles.imgSec}>
-								<img></img>
-							</div>
-							<div className={styles.imgDesc}></div>
-						</div>
-						<div className={styles.card}>
-							<h2>Card</h2>
-							<div className={styles.imgSec}>
-								<img></img>
-							</div>
-							<div className={styles.imgDesc}></div>
-						</div>
-						<div className={styles.card}>
-							<h2>Card</h2>
-							<div className={styles.imgSec}>
-								<img></img>
-							</div>
-							<div className={styles.imgDesc}></div>
-						</div>
-						<div className={styles.card}>
-							<h2>Card</h2>
-							<div className={styles.imgSec}>
-								<img></img>
-							</div>
-							<div className={styles.imgDesc}></div>
-						</div>
-						<div className={styles.card}>
-							<h2>Card</h2>
-							<div className={styles.imgSec}>
-								<img></img>
-							</div>
-							<div className={styles.imgDesc}></div>
-						</div>
+						{productBurger.map((i) => {
+							return (
+								<div className={styles.card}>
+									<div className={styles.Sec1}>
+										<h2 className={styles.title}>{i.title}</h2>
+										<div className={styles.prodInfo}>
+											<div className={styles.imgSec}>
+												<img src={i.img} alt='burger'></img>
+											</div>
+											<div className={styles.imgDesc}>{i.desc}</div>
+										</div>
+									</div>
+									<div className={styles.Sec2}>
+										<div className={styles.prodQty}>
+											<div className={styles.Qty}>
+												<div className={styles.add}>
+													<button>
+														<AiOutlinePlusCircle
+															style={{
+																fontSize: '30px',
+																border: '0px',
+															}}
+														/>
+													</button>
+												</div>
+												<div className={styles.reduce}>
+													<button>
+														<GrSubtractCircle
+															style={{
+																fontSize: '30px',
+																border: '0px',
+																padding: '0px',
+																margin: '0px',
+															}}
+														/>
+													</button>
+												</div>
+												<div className={styles.total}>Total</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
---
+++ Portfolio_FireItems/FoodOrderApp/food-order-app/src/data.js
@@ -0,0 +1,167 @@
+export const productBurger = [
+	{
+		id: 1,
+		title: 'Euphoria',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/PROJECTS_React/main/Portfolio_FireItems/FoodOrderApp/food-order-app/src/Img/Burger1.jpeg',
+		desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dignissimos, aliquid!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, iusto',
+	},
+
+	{
+		id: 2,
+		title: 'Virgo',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/PROJECTS_React/main/Portfolio_FireItems/FoodOrderApp/food-order-app/src/Img/Burger2.jpeg',
+		desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dignissimos, aliquid!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, iusto',
+	},
+	{
+		id: 3,
+		title: 'Virgo',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/PROJECTS_React/main/Portfolio_FireItems/FoodOrderApp/food-order-app/src/Img/Burger3.jpeg',
+		desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dignissimos, aliquid!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, iusto',
+	},
+	{
+		id: 4,
+		title: 'Paralle',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/PROJECTS_React/main/Portfolio_FireItems/FoodOrderApp/food-order-app/src/Img/Burger4.jpg',
+		desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dignissimos, aliquid!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, iusto',
+	},
+	{
+		id: 5,
+		title: 'Hangry',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/PROJECTS_React/main/Portfolio_FireItems/FoodOrderApp/food-order-app/src/Img/Burger5.jpeg',
+		desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dignissimos, aliquid!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, iusto',
+	},
+];
+
+export const TechData = [
+	{
+		id: 1,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/HTML5.png',
+		text: 'HTML',
+	},
+	{
+		id: 2,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/css3-logo-png-transparent.png',
+		text: 'CSS',
+	},
+	{
+		id: 3,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/Javascript.png',
+		text: 'JavaScript',
+	},
+	{
+		id: 4,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/sass-1-logo-png-transparent.png',
+		text: 'SASS',
+	},
+	{
+		id: 5,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/Bootstrap.png',
+		text: 'Bootstrap',
+	},
+	{
+		id: 6,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/ReactJS.png',
+		text: 'React',
+	},
+	{
+		id: 7,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/github.png',
+		text: 'GitHub',
+	},
+	{
+		id: 8,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/mySql1.png',
+		text: 'mySQL',
+	},
+	{
+		id: 9,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/MongoDB-logo.png',
+		text: 'MongoDB',
+	},
+	{
+		id: 10,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/StyledComponents1.png',
+		text: 'StyledComponent',
+	},
+	{
+		id: 11,
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_dte_10oct21/main/src/img/Tech-Images/materialUi.png',
+		text: 'MaterialUI',
+	},
+];
+
+export const linkedinData = [
+	{
+		key: 1,
+		id: 1,
+		name: 'Team',
+		title: 'Chrysalis HRD',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/chrysalis.png',
+		icon: 'assets/twitter.png',
+		desc: '"We thank Kunal for being part of the Best blended"learning project- "Workplace readiness" which was created and delivered as blended journey. The entire work was done virtually and it created a measurable impact. Chrysalis is proud of you! We wish you for your stellar performance in the years to come"',
+	},
+	{
+		key: 1,
+		id: 1,
+		name: 'Joby Abraham',
+		title: 'AVP, Bank Of America',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/Joby.jpeg',
+		icon: 'assets/twitter.png',
+		desc: '"His passion for learning and focus on finding solutions is contagious. He has a solution to every challenge, but does not drive to it himself alone; he ensures he pulls the team together with him towards it. He is the perfect ingredient for a winning team, who lives by the values he holds and never compromises on quality."',
+	},
+	{
+		key: 2,
+		id: 2,
+		name: 'Salman',
+		title: 'Manager - RateGain',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/Salman.jpeg',
+		icon: 'assets/youtube.png',
+		desc: `"He has the ability to connect with people and devise methods to enhance their potential which is why he is the best fit for any organization's training needs. There is no doubt that he is the right person to interact and develop the future leaders of any company."`,
+	},
+	{
+		key: 3,
+		id: 3,
+		name: 'Sarvocch M',
+		title: 'Implementation Specialist - Preferred Hotels & Resorts',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/Sarvoch.jpeg',
+		icon: 'assets/linkedin.png',
+		desc: `"Kunal's excellent teaching skills and courteous personality has helped me tremendously through my journey at RateGain. He is the best trainer I have ever come across."`,
+	},
+	{
+		key: 4,
+		id: 4,
+		name: 'Pankaj C',
+		title: 'Logistics Specialist, Dehavilland Aircrafts - Canada',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/pankaj.jpeg',
+		icon: 'assets/twitter.png',
+		desc: `"He proved himself to be very calm and composed during high pressure situations. He would be a great addition to any organization.He is a skilled professional and reliable. He possesses the knowledge to succeed in any undertaking. "`,
+	},
+	{
+		key: 5,
+		id: 5,
+		name: 'Pankaj Kumar',
+		title: 'Client Manager, American Express',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/pankajKumar.jpeg',
+		icon: 'assets/youtube.png',
+		desc: `"He has this string ability to go to the root cause of the situation and solve it for good rather than a quick fix to solve it for today and let it burn again tomorrow. He has a particularly impressive eye for detail.
+		I was particularly impressed by his ability to handle even the toughest situations - effortlessly."`,
+	},
+	{
+		key: 6,
+		id: 6,
+		name: 'Supriya M',
+		title: 'Sr. Manager - BCD Travels',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/Supriya.jpeg',
+		icon: 'assets/youtube.png',
+		desc: `"Kunal is an excellent trainer and mentor. He keeps the audience engaged and on their toes. I learnt an incredible lot about Travel and GDS from him. His trainings were fun and a learning experience at the same time."`,
+	},
+	{
+		key: 7,
+		id: 7,
+		name: 'Anupam Mukherji',
+		title: 'Manager - Hospitality',
+		img: 'https://raw.githubusercontent.com/contactKunalDixit/Portfolio_Responsive/main/src/img/Testimonials/Anupam.jpeg',
+		icon: 'assets/youtube.png',
+		desc: `"Kunal is crisp, dependable, hardworking, goal oriented, strong on people relationships, detail oriented and great with project management. Little wonder that he delivers!"`,
+	},
+];
